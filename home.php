74afbecef3fe082b81f08590645593aa266c6bf8
Dinesh Kumar <mapmydestination@gmail.com>
2024-09-03 07:59:53 +0530

    updated

---
+++ home.php
@@ -0,0 +1,50 @@
+
+    <section id="home-section" class="hero">
+          <div class="home-slider js-fullheight owl-carousel">
+          <div class="slider-item js-fullheight">
+            <div class="overlay"></div>
+            <div class="container-fluid p-0">
+              <div class="row d-md-flex no-gutters slider-text js-fullheight align-items-center justify-content-end" data-scrollax-parent="true">
+                <div class="one-third order-md-last img js-fullheight" style="background-image:url(admin/img/school2.jpg);">
+                </div>
+                  <div class="one-forth d-flex js-fullheight align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+                    <div class="text">
+                        <span class="subheading">BUK - Online Management System</span>
+                        <div class="horizontal">
+                            <h3 class="vr" style="background-image: url(images/divider.jpg);">Established 2020</h3>
+                            <h3 class="mb-4 mt-3">View Your Own <br><span>Records and Grades</span></h3>
+                            <p>"وفوق كل ذي علم عليم"</p>
+							<p>"...and above every possessor of knowledge there is </p>
+                            
+                            <p><a href="index.php?q=blog" class="btn btn-primary px-5 py-3 mt-3">Discover Now</a></p>
+                          </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+
+          <div class="slider-item js-fullheight">
+            <div class="overlay"></div>
+            <div class="container-fluid p-0">
+              <div class="row d-flex no-gutters slider-text js-fullheight align-items-center justify-content-end" data-scrollax-parent="true">
+                <div class="one-third order-md-last img js-fullheight" style="background-image:url(admin/img/school.jpg);">
+                </div>
+                  <div class="one-forth d-flex js-fullheight align-items-center ftco-animate" data-scrollax=" properties: { translateY: '70%' }">
+                    <div class="text">
+                        <span class="subheading">BUK -  Online Management System</span>
+                        <div class="horizontal">
+                            <h3 class="vr" style="background-image: url(images/divider.jpg);">BUK - Student Management System</h3>
+                            <h2 class="mb-4 mt-3">A Thouroughly <span>Modern</span> Management System</h2>
+                            <p>A small river named Duden flows by their place and <br> supplies it with the necessary regelialia.</p>
+							<p>It is a paradisematic country.</p>
+                            
+                            <p><a href="index.php?q=login" class="btn btn-primary px-5 py-3 mt-3">Get Started</a></p>
+                          </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+    </section>
